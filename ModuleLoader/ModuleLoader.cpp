967d200e205ac102cf5d9f3a562a56a0c123d4af
lxy <liuretell@gmail.com>
2025-03-10 22:47:25 +0800

    构建基础架构

--- ModuleLoader/ModuleLoader.cpp
+++ ModuleLoader/ModuleLoader.cpp
@@ -3,21 +3,21 @@
 
 namespace CHAT::ModuleLoader {
 void ModuleLoader::load() {
-    std::string configPath = Utils::FileUtils::fileSystem::currentPath() + "ModuleDefine.json";
-    if (!Utils::FileUtils::fileSystem::fileExists(configPath)) {
-        // todo : add log
-        return;
-    }
-    Utils::JsonUtils::json j = Utils::JsonUtils::json::parse(Utils::FileUtils::fileSystem::readFile(configPath));
-    for (auto& module : j["modules"]) {
-        std::string moduleName = module["name"];
-        std::string moduleType = module["type"];
-        if (moduleType == "dll") {
-            // todo : load dll
-        } else if (moduleType == "so") {
-            // todo : load so
-        }
-    }
+    // std::string configPath = Utils::FileUtils::fileSystem::currentPath() + "ModuleDefine.json";
+    // if (!Utils::FileUtils::fileSystem::fileExists(configPath)) {
+    //     // todo : add log
+    //     return;
+    // }
+    // Utils::JsonUtils::json jsonObject = Utils::JsonUtils::json::parse(Utils::FileUtils::fileSystem::readFile(configPath));
+    // for (auto& module : jsonObject["modules"]) {
+    //     std::string moduleName = module["name"];
+    //     std::string moduleType = module["type"];
+    //     if (moduleType == "dll") {
+    //         // todo : load dll
+    //     } else if (moduleType == "so") {
+    //         // todo : load so
+    //     }
+    // }
     
 }
 }
